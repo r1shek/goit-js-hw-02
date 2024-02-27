9ba3e16d61e37db541f09b559be309d6fa798754
r1shek <rostic523@gmail.com>
2024-02-27 18:16:14 +0100

    2

--- js/task-4.js
+++ js/task-4.js
@@ -4,19 +4,15 @@ function getShippingCost(country) {
     case 'China':
       price = 100;
       return `Shipping to ${country} will cost ${price} credits`;
-      break;
     case 'Chile':
       price = 250;
       return `Shipping to ${country} will cost ${price} credits`;
-      break;
     case 'Australia':
       price = 170;
       return `Shipping to ${country} will cost ${price} credits`;
-      break;
     case 'Jamaica':
       price = 120;
       return `Shipping to ${country} will cost ${price} credits`;
-      break;
     default:
       return 'Sorry, there is no delivery to your country';
   }
